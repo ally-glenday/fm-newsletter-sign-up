37db93289fac5fc66bb89cbecdf664cb211b1956
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2023-10-13 10:13:00 +0900

    Link script with HTML, link JS with 'onsubmit'

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 /* Function to validate email using regular expression */ 
 
 function validateEmail (){
+    /* Create a regex variable for checking if input is valid */ 
     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;  
+    /* Create a variable to store user's input */ 
+    const userInput = document.getElementById("user-input");
 
-    
+
+    document.querySelector("");
+
 
 }
+
+
+
+/* If 
+
+user types in an invalid address
+
+OR 
+
+there is no input 
+
+*/ 
