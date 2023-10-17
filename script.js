b6f86b0f16d58188b394b7dc031213abf2ebd756
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2023-10-17 14:49:55 +0900

    Change variables to var

--- script.js
+++ script.js
@@ -1,89 +1,54 @@
 /* Function to validate email using regular expression */ 
-
 function validateEmail (){
-    /* Create a regex variable for checking if input is valid */ 
-    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;  
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;  
     /* Create a variable to store user's input */ 
-    let userInput = document.getElementById("user-input");
-
-    // const para = document.createElement("p");
-    // const node = document.createTextNode("Please enter valid email");
-    // para.appendChild(node);
-
-    // const validEmailMessage = document.getElementById("email-form");
-    // const childElement = document.querySelector("form > p ")
-
-    // validEmailMessage.insertBefore(para, childElement);
-
-
+    var userInput = document.getElementById("user-input");
+    const placeholder = document.querySelector("placeholder");
+    
+    var form = document.querySelector('form');
+    var emailAlert = document.createElement("p");
+    emailAlert.textContent = "Valid email required";
+    form.prepend(emailAlert);
 
     
-    // .insertBefore(para, child)
-
-
-
+    if(userInput != validRegex || userInput == ""){
+   
+    
+    /* Alert message styling (Valid email required) */
+    emailAlert.style.float = "right";
+    emailAlert.style.fontSize = ".8rem";
+    emailAlert.style.color = "#FF6257";
+    emailAlert.style.fontWeight = "bold";
+    
+    /* Email address / user input styling */
+    userInput.style.backgroundColor = "#ffe8e6";
+    userInput.style.border = "solid #FF6257 1px";
+    userInput:placeholder.style.border = "solid #FF6257 1px";
+        validateEmail();
+        return false;
+          
+     }
+    
 
 }
 
-/* Create a p element */ 
-// const para = document.createElement("p");
-// /* Assign the text 'please enter a valid email' to that p tag */ 
-// const node = document.createTextNode("Please enter valid email");
-// /* Combine these two together */ 
-// para.appendChild(node);
 
-// const validEmailMessage = document.getElementById("email-form");
-// const childElement = document.querySelector("form > label ");
 
-// validEmailMessage.insertBefore(para, childElement);
 
-// validEmailMessage.style.fontSize = "2em";
-
-// document.querySelector("form > p").style.outline = "thick solid #0000FF";
-// document.querySelector("form > p").style.fontSize = "1rem";
-// document.querySelector
-
-const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;  
-/* Create a variable to store user's input */ 
-let userInput = document.getElementById("user-input");
-const placeholder = document.querySelector("placeholder");
-
-const form = document.querySelector('form');
-const emailAlert = document.createElement("p");
-emailAlert.textContent = "Valid email required";
-form.prepend(emailAlert);
-
-/* Alert message styling (Valid email required) */
-emailAlert.style.float = "right";
-emailAlert.style.fontSize = ".8rem";
-emailAlert.style.color = "#FF6257";
-emailAlert.style.fontWeight = "bold";
-
-/* Email address / user input styling */
-userInput.style.backgroundColor = "#ffe8e6";
-userInput.style.border = "solid #FF6257 1px";
-userInput:placeholder.style.border = "solid #FF6257 1px";
+// function validateForm() {
+//     let x = document.forms["myForm"]["fname"].value;
+//     if (x == "") {
+//       alert("Name must be filled out");
+//       return false;
+//     }
+//   } 
 
 
 
 
-// const div = document.querySelector('div')
-// const newChild = document.createElement('p')
-// newChild.textContent = 'foo'
-// div.prepend(newChild)
-
-// emailAlert.prepend()
-
-// div.prepend(newChild)
 
 
 
-/* If 
 
-user types in an invalid address
 
-OR 
-
-there is no input 
-
-*/ 
