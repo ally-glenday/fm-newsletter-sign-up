d636ff0c3dac98405d0d3f522603be184c1eaf91
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2023-10-17 17:21:13 +0900

    Working (form linked with submit)

--- script.js
+++ script.js
@@ -1,52 +1,122 @@
-/* Function to validate email using regular expression */ 
-function validateEmail (){
+// function successMessage(){
+//     alert("Success!");
+// }
 
+function validateEmail(){
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;  
     /* Create a variable to store user's input */ 
     var userInput = document.getElementById("user-input");
-    const placeholder = document.querySelector("placeholder");
-    
-    var form = document.querySelector('form');
-    var emailAlert = document.createElement("p");
-    emailAlert.textContent = "Valid email required";
-    form.prepend(emailAlert);
+    var placeholder = document.querySelector("placeholder");
 
-    
-    if(userInput != validRegex || userInput == ""){
+    alert("hey");
+
+
    
-    
-    /* Alert message styling (Valid email required) */
-    emailAlert.style.float = "right";
-    emailAlert.style.fontSize = ".8rem";
-    emailAlert.style.color = "#FF6257";
-    emailAlert.style.fontWeight = "bold";
-    
-    /* Email address / user input styling */
-    userInput.style.backgroundColor = "#ffe8e6";
-    userInput.style.border = "solid #FF6257 1px";
-    userInput:placeholder.style.border = "solid #FF6257 1px";
-        validateEmail();
-        return false;
-          
-     }
-    
-
 }
 
+// var form = document.getElementById('email-form');
+// var emailAlert = document.createElement("p");
+// emailAlert.style.float = "right";
+// emailAlert.style.fontSize = ".8rem";
+// emailAlert.style.color = "#FF6257";
+// emailAlert.style.fontWeight = "bold";
+// emailAlert.textContent = "Valid email required";
+
+
+// userInput.style.backgroundColor = "#ffe8e6";
+// userInput.style.border = "solid #FF6257 1px";
+// placeholder.style.border = "solid #FF6257 1px";
+
+
+// form.prepend(emailAlert); 
 
 
 
-// function validateForm() {
-//     let x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//       alert("Name must be filled out");
-//       return false;
+
+/* Function to validate email using regular expression */ 
+// function validateEmail(){
+
+//     do{
+//         emailAlert.style.float = "right";
+//         emailAlert.style.fontSize = ".8rem";
+//         emailAlert.style.color = "#FF6257";
+//         emailAlert.style.fontWeight = "bold";
+//         emailAlert.textContent = "Valid email required";
+//         form.prepend(emailAlert); 
+//         return false;
 //     }
-//   } 
+    
+//     while(userInput !== validRegex || userInput == "");
+    
+    
+   
+
+// function ValidateEmail(mail) 
+// {
+//  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+//   {
+//     return (true)
+//   }
+//     alert("You have entered an invalid email address!")
+//     return (false)
+
+
+// }
 
 
 
 
+
+
+
+
+
+
+/* 
+
+ /* Alert message styling (Valid email required) */
+    // emailAlert.style.float = "right";
+    // emailAlert.style.fontSize = ".8rem";
+    // emailAlert.style.color = "#FF6257";
+    // emailAlert.style.fontWeight = "bold";
+
+    
+    
+    /* Email address / user input styling */
+    // userInput.style.backgroundColor = "#ffe8e6";
+    // userInput.style.border = "solid #FF6257 1px";
+    // placeholder.style.border = "solid #FF6257 1px";
+
+
+
+
+
+
+
+
+
+
+
+// IF
+
+// button is clicked with no input 
+
+// i.e. userInput = "";
+
+// OR 
+
+// userInput is not valid 
+
+// i.e. userInput != regex 
+
+// THEN
+
+// return emailAlert 
+
+
+
+
+// */
 
 
 
